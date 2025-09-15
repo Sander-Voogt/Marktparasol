d1c606203c4e408a665d83c9d3b19be506e3eeb8
Sander Voogt <sandervoogt@outlook.com>
2025-09-15 20:35:46 +0200

    update

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -32,6 +32,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
+            { // Note: can be removed after deleting demo products
+        protocol: "https",
+        hostname: "form.evservice.eu",
+      },
       { // Note: can be removed after deleting demo products
         protocol: "https",
         hostname: "medusa-server-testing.s3.amazonaws.com",
