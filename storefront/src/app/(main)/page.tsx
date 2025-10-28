42f542c880aa93f27c7f8ca2e090999a4a0ba884
Sander Voogt <sandervoogt@outlook.com>
2025-10-28 15:18:14 +0100

    update

--- storefront/src/app/(main)/page.tsx
+++ storefront/src/app/(main)/page.tsx
@@ -6,9 +6,9 @@ import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Sterke Marktparasols in verschillende maten",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Koop uw marktparasols bij markt-parasol.nl. Voorzie uw parasol van logo of tekst.",
 }
 
 export default async function Home({
