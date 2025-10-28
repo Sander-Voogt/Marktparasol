8a7b5cd3db3abee552e69cf46c35132a44f9db79
Sander Voogt <sandervoogt@outlook.com>
2025-10-28 15:23:02 +0100

    update

--- storefront/src/app/(main)/layout.tsx
+++ storefront/src/app/(main)/layout.tsx
@@ -12,6 +12,7 @@ export default async function PageLayout(props: { children: React.ReactNode }) {
   return (
     <>
       <Nav />
+      <div className="bg-green-700 text-white text-center">Bel met Sander voor vragen <a href="tel:+31617360645">0617360645</a></div>
       {props.children}
       <Footer />
     </>
