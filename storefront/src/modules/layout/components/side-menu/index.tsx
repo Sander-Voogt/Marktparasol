be6003af0d6ad9b92ccf78706719222cc6a2dceb
Sander Voogt <sandervoogt@outlook.com>
2025-11-04 20:38:38 +0100

    updates

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -11,7 +11,7 @@ import { HttpTypes } from "@medusajs/types"
 
 const SideMenuItems = {
   Home: "/",
-  Parasols: "/store",
+  Parasols: "/collections/markt-parasols",
   Zoeken: "/search",
   Offerte: "/offerte-aanvragen",
   Account: "/account",
