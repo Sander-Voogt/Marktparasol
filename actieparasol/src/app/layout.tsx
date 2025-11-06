4df3b5eee70d2c7cb5f9ac92ab86bd79b7dc74eb
Sander Voogt <sandervoogt@outlook.com>
2025-11-06 22:06:47 +0100

    update

--- actieparasol/src/app/layout.tsx
+++ actieparasol/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
+import { GoogleTagManager } from "@next/third-parties/google"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -9,8 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
-            <GoogleTagManager gtmId="GTM-W3XXPBL3" gtmScriptUrl="https://server-side-tracking-production-c7d9.up.railway.app/gtm.js" />
-      
+      <GoogleTagManager
+        gtmId="GTM-N76LT4GL"
+      />
+
       <body>
         <main className="relative">{props.children}</main>
       </body>
