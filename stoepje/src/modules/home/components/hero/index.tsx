541730be5d02eefee0c8bc94bd5a9f7e394f6f06
Sander Voogt <sandervoogt@outlook.com>
2025-12-02 22:04:25 +0100

    tekst verwijderd

--- stoepje/src/modules/home/components/hero/index.tsx
+++ stoepje/src/modules/home/components/hero/index.tsx
@@ -7,29 +7,12 @@ const Hero = () => {
         <div className="flex flex-col md:flex-row">
           <div className="basis-1/2 m-8">
             <h3 className="text-5xl mb-8 mt-8">
-              Sterke Marktparasols Nodig?
+              Bestel eenvoudig producten met het logo van stoepje. 
             </h3>
-            <p className="text-xl leading-7 ">
-              Op zoek naar een stevige en duurzame marktparasol?
-              <br />
-              Onze hoogwaardige parasols bieden optimale bescherming en zijn
-              eenvoudig in te klappen, zodat je snel en efficiënt kunt werken.
-              <br />
-              Dankzij de robuuste constructie en weerbestendige materialen ben
-              je verzekerd van een lange levensduur. <br />
-              Kies voor kwaliteit en gemak met onze marktparasols!
-            </p>
-            <button className="text-white text-xl font-semibold mt-8 bg-yellowGreen hover:bg-amber-500 focus:ring-4 focus:ring-amber-600 font-medium rounded-sm text-sm px-5 p-2 me-2 mb-2 ">
-              Koop parasol
-            </button>
+            
           </div>
           <div className="basis-1/2 items-center">
-            <img
-              src="https://markt-parasol.nl/img/marktparasols-groen-wit.jpg"
-              alt=""
-              height={600}
-              width={600}
-            />
+            
           </div>
         </div>
       </div>
