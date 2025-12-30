cf8aa3f641a0249d8587a80c2b3535c2b87e908c
Sander Voogt <sandervoogt@outlook.com>
2025-12-30 16:55:58 +0100

    footer aangepast

--- storefront/src/modules/layout/templates/footer/index.tsx
+++ storefront/src/modules/layout/templates/footer/index.tsx
@@ -22,7 +22,7 @@ export default async function Footer() {
             </LocalizedClientLink>
           </div>
           <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
-            {product_categories && product_categories?.length > 0 && (
+            {/* {product_categories && product_categories?.length > 0 && (
               <div className="flex flex-col gap-y-2">
                 <span className="txt-small-plus txt-ui-fg-base">
                   Categories
@@ -79,7 +79,7 @@ export default async function Footer() {
                   })}
                 </ul>
               </div>
-            )}
+            )} */}
             {/* {collections && collections.length > 0 && ( */}
             <div className="flex flex-col gap-y-2">
               <span className="txt-small-plus txt-ui-fg-base"></span>
