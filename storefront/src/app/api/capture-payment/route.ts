bf64fad0637871430f5a32ae4e1c64b4b711ff03
Sander Voogt <Sander Voogt>
2026-03-31 15:04:27 +0200

    mollie

---
+++ storefront/src/app/api/capture-payment/route.ts
@@ -0,0 +1,44 @@
+import { placeOrder, retrieveCart } from "@lib/data/cart"
+import { NextRequest, NextResponse } from "next/server"
+import { redirect } from "next/navigation"
+
+type Params = Promise<{ cartId: string }>
+
+export async function GET(req: NextRequest, { params }: { params: Params }) {
+  const { searchParams } = req.nextUrl
+
+  const origin = process.env.NEXT_PUBLIC_BASE_URL
+  const paymentIntent = searchParams.get("payment_intent")
+  const paymentIntentClientSecret = searchParams.get(
+    "payment_intent_client_secret"
+  )
+  const redirectStatus = searchParams.get("redirect_status") || ""
+  const countryCode = searchParams.get("country_code")
+
+  const cart = await retrieveCart()
+
+  if (!cart) {
+    return redirect(`${origin}/${countryCode}`)
+  }
+
+  const paymentSession = cart.payment_collection?.payment_sessions?.find(
+    (payment) => payment.data.id === paymentIntent
+  )
+
+  if (
+    !paymentSession ||
+    paymentSession.data.client_secret !== paymentIntentClientSecret ||
+    !["pending", "succeeded"].includes(redirectStatus) ||
+    !["pending", "authorized"].includes(paymentSession.status)
+  ) {
+    return redirect(
+      `${origin}/${countryCode}/checkout/return?cart_id=${cart.id}`
+    )
+  }
+
+  // const order = await placeOrder(cart.id)
+
+  return NextResponse.redirect(
+    `${origin}/${countryCode}/checkout/return?cart_id=${cart.id}`
+  )
+}
