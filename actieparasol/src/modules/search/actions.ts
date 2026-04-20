475b47614db9e3f196724775a8d9663b0b8ac4a7
Sander Voogt <Sander-Voogt@users.noreply.github.com>
2026-04-20 11:55:26 +0200

    Update actions.ts

--- actieparasol/src/modules/search/actions.ts
+++ actieparasol/src/modules/search/actions.ts
@@ -8,30 +8,25 @@ interface Hits {
   [x: string | number | symbol]: unknown
 }
 
-const MEDUSA_STORE_SALES_CHANNEL_ID = 'sc_01K5RS8ARYVAAAMA4BFMBMZH0T'
+const MEDUSA_STORE_SALES_CHANNEL_ID = "sc_01K5RS8ARYVAAAMA4BFMBMZH0T"
 
-/**
- * Uses MeiliSearch or Algolia to search for a query
- * @param {string} query - search query
- */
 export async function search(query: string) {
-  // MeiliSearch
-  const queries = [{ params: { query }, indexName: SEARCH_INDEX_NAME }]
-  const { results } = (await searchClient.search(queries,{
-  filter: [
-    `sales_channels.id = "${STORE_SALES_CHANNEL_ID}"`,
-    `status = published`
+  const queries = [
+    {
+      indexName: SEARCH_INDEX_NAME,
+      params: {
+        query,
+        filter: [
+          `sales_channels.id = "${MEDUSA_STORE_SALES_CHANNEL_ID}"`,
+          `status = published`,
+        ],
+      },
+    },
   ]
-})) as Record<
-    string,
-    any
-  >
+
+  const { results } = await searchClient.search(queries)
+
   const { hits } = results[0] as { hits: Hits[] }
-
-  // In case you want to use Algolia instead of MeiliSearch, uncomment the following lines and delete the above lines.
-
-  // const index = searchClient.initIndex(SEARCH_INDEX_NAME)
-  // const { hits } = (await index.search(query)) as { hits: Hits[] }
 
   return hits
 }
